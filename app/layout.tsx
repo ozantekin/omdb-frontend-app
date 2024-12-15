f606f156ccf90bc38c8b356ed8cf59c92cde9e60
Ozan Tekin <ardilozantekin@gmail.com>
2024-12-15 13:15:51 +0300

    feat(app): add mobie structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import RTK from "@omdb/providers/rtk";
 
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "@omdb/styles/index.scss";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,8 +10,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: ChildrenProps) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" suppressHydrationWarning>
+      <body>
         <RTK>{children}</RTK>
       </body>
     </html>
--- app/movie/page.tsx
+++ app/movie/page.tsx
@@ -1,5 +1,10 @@
 import MoviesTable from "@omdb/modules/movies-table";
+import { Suspense } from "react";
 
 export default function MoviePage() {
-  return <MoviesTable />;
+  return (
+    <Suspense fallback="Loading...">
+      <MoviesTable />
+    </Suspense>
+  );
 }
