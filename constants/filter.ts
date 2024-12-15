28647fc8573d7f9d9a43a71d2b5ffff70744886a
Ozan Tekin <ardilozantekin@gmail.com>
2024-12-15 13:14:08 +0300

    feat: add constanst

---
+++ constants/filter.ts
@@ -0,0 +1,40 @@
+export const YEAR_OPTIONS = [
+  {
+    label: "All years",
+    value: "",
+  },
+  {
+    label: "2021",
+    value: "2021",
+  },
+  {
+    label: "2020",
+    value: "2020",
+  },
+  {
+    label: "2019",
+    value: "2019",
+  },
+  {
+    label: "2018",
+    value: "2018",
+  },
+  {
+    label: "2017",
+    value: "2017",
+  },
+  {
+    label: "2016",
+    value: "2016",
+  },
+  {
+    label: "2015",
+    value: "2015",
+  },
+  {
+    label: "2014",
+    value: "2014",
+  },
+];
+
+export const TYPE_OPTIONS: TypeProps[] = ["movie", "series", "episode"];
